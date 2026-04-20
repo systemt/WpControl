07cdc9c782c1e705cfa7170c89ba8c32f51351a5
Omer Sade <systemt@gmail.com>
2026-04-20 13:16:25 +0300

    set node version to 20

--- digiforce-central/src/modules/auth/auth.service.ts
+++ digiforce-central/src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { prisma } from '../../lib/prisma';
 import { signToken } from '../../lib/jwt';
 import { ApiError } from '../../utils/api-error';
